cb033c0c96f1c84dad2e275ef31b0ed54b591553
preston-huang <103078343+preston-huang@users.noreply.github.com>
2023-03-11 19:15:33 -0800

    Update to most recent version

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <kipr/wombat.h>
 
+//constants
 int LEFT_MOTOR_PORT = 0;
 int RIGHT_MOTOR_PORT = 1;
 
@@ -23,12 +24,29 @@ float LINE_FOLLOW_SENSITIVITY_HIGH = 0.5;
 float TICKS_PER_DEGREE = 13.1;
 float TICKS_PER_INCH = 222.2;
 
-void drive_forward(float distance, int speed){
+int LIFT_UP = 324;
+int LIFT_DOWN = 2047;
+int LIFT_PORT = 0;
+
+int SORTER_LEFT = 0;
+int SORTER_RIGHT = 1458;
+int SORTER_MIDDLE = 694;
+int SORTER_PORT = 1;
+
+void drive(float distance, int speed){
     cmpc(LEFT_MOTOR_PORT);
     cmpc(RIGHT_MOTOR_PORT);
-    while (gmpc(LEFT_MOTOR_PORT) < distance*TICKS_PER_INCH){
-        mav(LEFT_MOTOR_PORT, speed);
-        mav(RIGHT_MOTOR_PORT, speed);
+    if(distance > 0){
+        while (gmpc(LEFT_MOTOR_PORT) < distance*TICKS_PER_INCH){
+            mav(LEFT_MOTOR_PORT, speed);
+            mav(RIGHT_MOTOR_PORT, speed);
+        }
+    }
+    else {
+        while (gmpc(LEFT_MOTOR_PORT) > distance*TICKS_PER_INCH){
+            mav(LEFT_MOTOR_PORT, -speed);
+            mav(RIGHT_MOTOR_PORT, -speed);
+        }
     }
     mav(RIGHT_MOTOR_PORT, 0);
     mav(LEFT_MOTOR_PORT, 0);
@@ -44,6 +62,7 @@ void turn_right(int degrees, int speed){
     mav(RIGHT_MOTOR_PORT, 0);
     mav(LEFT_MOTOR_PORT, 0);
 }    
+
 void turn_left(int degrees, int speed){
 	cmpc(LEFT_MOTOR_PORT);
     cmpc(RIGHT_MOTOR_PORT);
@@ -79,8 +98,8 @@ void line_follow(float distance, int speed, int is_left, int port, float sensiti
     }
     mav(RIGHT_MOTOR_PORT, 0);
     mav(LEFT_MOTOR_PORT, 0);
-    
 }
+
 void line_follow_backwards(float distance, int speed, int is_left, int port, float sensitivity){
     float percentage;
     int mid_color = (SMALL_BLACK_VALUE + SMALL_WHITE_VALUE)/2;
@@ -107,11 +126,19 @@ void line_follow_backwards(float distance, int speed, int is_left, int port, flo
     mav(LEFT_MOTOR_PORT, 0);
 }
 
-
-void move_servo(int port, int position, int time) {
+void move_servo(int port, int target_position, int target_time) {
+    int interval = 10;
+    int current_position = get_servo_position(port);
+    int current_time = 0;
+    while (current_time<target_time) {
+       	set_servo_position(port, current_position + (current_time*(target_position-current_position)/target_time));
+       	msleep(interval);
+       	current_time = current_time + interval;
+    }
+    set_servo_position(port, target_position);
 }
 
-void move_claw(int target, int speed) {
+void close_claw(int target, int speed) {
     while (gmpc(CLAW_PORT) < target){
         mav(CLAW_PORT, speed);
         msleep(100);
@@ -119,22 +146,43 @@ void move_claw(int target, int speed) {
     mav(CLAW_PORT, 0);
 }
 
+void open_claw(int target, int speed) {
+    while (gmpc(CLAW_PORT) > target){
+        mav(CLAW_PORT, -speed);
+        msleep(100);
+    }
+    mav(CLAW_PORT, 0);
+}
+
+void calibrate(){
+    move_servo(LIFT_PORT, LIFT_UP, 1000);
+    move_servo(SORTER_PORT, SORTER_MIDDLE, 500);
+    open_claw(0, 1000);
+}
+
 int main()
 {
-    /*cmpc(CLAW_PORT);
-    drive_forward(11, 1000);
-    turn_left(85, 1000);
-    move_claw(CLAW_ATTACK, 1000);
-    line_follow_backwards(29.0, 1000, 1, BACK_TOPHAT_PORT, LINE_FOLLOW_SENSITIVITY_LOW);
-    move_claw(CLAW_CLOSED, 1000);
-    line_follow(58, 1000, 0, RIGHT_TOPHAT_PORT, LINE_FOLLOW_SENSITIVITY_LOW);
+    enable_servos();
+    calibrate();
+    
+    // scoring hella points
+    drive(-2,1000);
+    turn_left(3,400);
+    move_servo(LIFT_PORT, LIFT_DOWN, 1000);
+    drive(11, 1000);
+    move_servo(SORTER_PORT, SORTER_RIGHT, 500);
+    turn_right(85, 1000);
+    move_servo(SORTER_PORT, SORTER_LEFT, 500);
+    line_follow(6, 1000, 1, LEFT_TOPHAT_PORT, LINE_FOLLOW_SENSITIVITY_LOW);
+    move_servo(SORTER_PORT, SORTER_RIGHT, 500);
+    line_follow(8, 1000, 1, LEFT_TOPHAT_PORT, LINE_FOLLOW_SENSITIVITY_LOW);
+    move_servo(SORTER_PORT, SORTER_LEFT, 500);    
+    close_claw(CLAW_ATTACK, 1000);
     turn_left(180, 1000);
-    line_follow_backwards(15.0, 400, 1, BACK_TOPHAT_PORT, LINE_FOLLOW_SENSITIVITY_LOW);
-    drive_forward(1, 400);
-    msleep(10000);
-    line_follow(7, 1000, 1, LEFT_TOPHAT_PORT, LINE_FOLLOW_SENSITIVITY_LOW);
-    turn_left(90, 1000);
-*/
-    drive_forward(15,1000);
+    line_follow_backwards(10, 1000, 0, BACK_TOPHAT_PORT, LINE_FOLLOW_SENSITIVITY_LOW);
+    close_claw(CLAW_CLOSED, 1000);
+    
+    
+   
     return 0;
 }
